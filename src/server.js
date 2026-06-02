470788c20ae2308bbac4aae1b9854ff0dd8eb40a
MarioMS2000 <92580217+MarioMS2000@users.noreply.github.com>
2026-06-02 13:48:24 +0200

    Setup Express server and health endpoint

---
+++ src/server.js
@@ -0,0 +1,16 @@
+// Archivo que arranca el servidor. Mientras que app.js configura Express, server.js es el que realmente lo pone a escuchar peticiones
+
+import dotenv from "dotenv"; // Importa libreria para cargar las variables que tengas en tu archivo .env
+import app from "./app.js"; // Importo la aplicación que cree en app.js y la recuperamos para arrancarla
+
+dotenv.config(); // Lee el archivo .env y mete sus variables dentro
+
+const PORT = process.env.PORT || 3000; // Obtenemos el puerto. Usa el puerto definido en .env. Si no existe, usa el 3000 y si exite pues el que hay en el .env
+
+// Arrancar el servidor
+app.listen(PORT, () => {
+    app.listen(5000);
+    console.log(
+        `🚀 Server running on port ${PORT}`
+    );
+});
