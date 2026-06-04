9b55b8e9625d3da59fee433895fab49f5cc797fc
MarioMS2000 <92580217+MarioMS2000@users.noreply.github.com>
2026-06-04 19:16:43 +0200

    Add role middleware

--- src/middlewares/role.middleware.js
+++ src/middlewares/role.middleware.js
@@ -1,9 +1,7 @@
 export const roleMiddleware = (...roles) => {
     return (req, res, next) => {
 
-        if (
-            !roles.includes(req.user.role)
-        ) {
+        if (!roles.includes(req.user.role)) {
             return res.status(403).json({
                 message: "No autorizado",
             });
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -3,6 +3,7 @@ import { register, login, logout, } from "../controllers/auth.controller.js";
 import { authMiddleware } from "../middlewares/auth.middleware.js"; // middleware de autenticación
 import { registerValidation, loginValidation, } from "../validations/auth.validation.js";
 import { validationMiddleware, } from "../middlewares/validation.middleware.js";
+import { roleMiddleware } from "../middlewares/role.middleware.js";
 
 const router = Router(); // Creas un mini servidor de rutas
 
@@ -19,5 +20,14 @@ router.get("/me", authMiddleware, (req, res) => {
         user: req.user,
     }); // Devolvemos los datos del user y viene el req.user del middleware
 });
+
+// Ruta temporal GET http://localhost:3000/api/auth/admin-test
+router.get("/admin-test", authMiddleware, roleMiddleware("admin"), (req, res) => {
+        res.json({
+            message: "Ruta admin funcionando",
+            user: req.user,
+        });
+    }
+);
 
 export default router;
